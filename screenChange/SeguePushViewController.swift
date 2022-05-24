ebf368cf77ffdb94c7c9c8316f8d0df9546fcbd2
js10081 <js100810081@gmail.com>
2022-05-24 17:40:21 +0900

    UI만 그려놓고 cocoa class 파일만 만들어 놓은 상태

--- screenChange/SeguePushViewController.swift
+++ screenChange/SeguePushViewController.swift
@@ -12,23 +12,6 @@ class SegueViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("SegueViewController 뷰가 로드 되었다.")
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        print("SegueViewController 뷰가 나타날 것이다.")
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        print("SegueViewController 뷰가 나타났다.")
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        print("SegueViewController 뷰가 사라질 것이다.")
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        print("SegueViewController 뷰가 사라졌다.")
     }
     
 }
--- screenChange/ViewController.swift
+++ screenChange/ViewController.swift
@@ -12,23 +12,6 @@ class ViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("ViewController 뷰가 로드되었다.")
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        print("ViewController 뷰가 나타날 것이다.")
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        print("ViewController 뷰가 나타났다.")
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        print("ViewController 뷰가 사라질 것이다.")
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        print("ViewController 뷰가 사라졌다.")
     }
 }
 
